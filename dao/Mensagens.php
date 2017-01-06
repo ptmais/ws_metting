1a0b7ede9eea73f066ade229ff4c8f8933b63861
ptmais <alexandre.aprendiz@gmail.com>
2017-01-06 10:58:13 -0200

    define todas as mensagens de retorno do servidor para o cliente

---
+++ dao/Mensagens.php
@@ -0,0 +1,24 @@
+<?php
+
+define('USUARIO_INSERIDO_SUCESSO', 'Conta cadastrada com sucesso.');
+define('USUARIO_DELETADO_SUCESSO', 'Conta deletada com sucesso.');
+define('USUARIO_ATUALIZADO_SUCESSO', 'Conta atualizada com sucesso.');
+define('USUARIO_NAO_ENCONTRADO', 'Nenhum usuário encontrado.');
+
+define('EVENTO_INSERIDO_SUCESSO', 'Evento cadastrado com sucesso.');
+define('EVENTO_DELETADO_SUCESSO', 'Evento deletado com sucesso.');
+define('EVENTO_ATUALIZADO_SUCESSO', 'Evento atualizado com sucesso.');
+define('EVENTO_NAO_ENCONTRADO', 'Nenhum evento encontrado.');
+
+define('EVENTOCONFIRMADO_INSERIDO_SUCESSO', 'Evento confirmado com sucesso.');
+define('EVENTOCONFIRMADO_DELETADO_SUCESSO', 'Evento cancelado com sucesso.');
+define('EVENTOCONFIRMADO_NAO_ENCONTRADO', 'Nenhum evento encontrado.');
+
+define('CATEGORIA_INSERIDO_SUCESSO', 'Categoria cadastrada com sucesso.');
+define('CATEGORIA_DELETADO_SUCESSO', 'Categoria deletada com sucesso.');
+define('CATEGORIA_ATUALIZADO_SUCESSO', 'Categoria atualizada com sucesso.');
+
+define('GRUPO_INSERIDO_SUCESSO', 'Grupo inserido com sucesso.');
+define('GRUPO_DELETADO_SUCESSO', 'Grupo deletado com sucesso.');
+define('GRUPO_ATUALIZADO_SUCESSO', 'Grupo atualizado com sucesso.');
+define('GRUPO_NAO_ENCONTRADO', 'Nenhuma grupo encontrado.');
